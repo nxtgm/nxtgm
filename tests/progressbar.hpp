0d67b2c47d2183df276524145695adcfd116b06c
DerThorsten <derthorstenbeier@gmail.com>
2023-08-02 15:55:10 +0200

    fixed win issues

--- tests/progressbar.hpp
+++ tests/progressbar.hpp
@@ -168,7 +168,7 @@ inline void progressbar::update()
         // erase the correct  number of characters
         if (perc <= 10)
             output << "\b\b" << perc << '%';
-        else if (perc > 10 and perc < 100)
+        else if (perc > 10 && perc < 100)
             output << "\b\b\b" << perc << '%';
         else if (perc == 100)
             output << "\b\b\b" << perc << '%';
