20b3e312b50802be5e15ad510c65143853fa35b2
DerThorsten <derthorstenbeier@gmail.com>
2023-08-16 12:52:20 +0200

    tests seem to work

--- src/nxtgm/optimizers/gm/discrete/discrete_gm_optimizer_factory.cpp
+++ src/nxtgm/optimizers/gm/discrete/discrete_gm_optimizer_factory.cpp
@@ -8,7 +8,6 @@ namespace nxtgm
 std::unique_ptr<DiscreteGmOptimizerBase> discrete_gm_optimizer_factory(const DiscreteGm &gm, const std::string &name,
                                                                        const nlohmann::json parameter)
 {
-    std::cout << "create solver " << name << std::endl;
     return get_plugin_registry<DiscreteGmOptimizerFactoryBase>().get_factory(name)->create(gm, parameter);
 }
 
