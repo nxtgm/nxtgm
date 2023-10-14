c0c71fb1c6755888f356f353e8122280497b3410
DerThorsten <derthorstenbeier@gmail.com>
2023-10-14 09:36:37 +0200

    removed test spec

--- javascript/ts_src/nxtgm.spec.ts
+++
@@ -1,9 +0,0 @@
-import { create_module } from "./index";
-
-beforeEach((): void => {
-  jest.setTimeout(60000);
-});
-
-test('the data is peanut butter', async () => {
-  await create_module();
-});
